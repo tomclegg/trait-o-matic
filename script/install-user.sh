f5ee66cffdc5f60272571667de1e83159235c53f
Tom Clegg <tom@tomclegg.net>
2009-09-03 11:05:53 -0400

    tweak install procedure to preserve config

--- script/install-user.sh
+++ script/install-user.sh
@@ -4,3 +4,17 @@ set -e
 
 ./setup-www.sh
 #./setup-external-data.sh
+
+SOURCE=$HOME/trait-o-matic
+if cp -i $SOURCE/core/config.default.py $SOURCE/core/config.py
+then
+  echo >&2 "*** "
+  echo >&2 "*** Please edit $SOURCE/core/config.py to suit your installation."
+  echo >&2 "*** "
+else
+  echo >&2 "*** "
+  echo >&2 "*** Please ensure $SOURCE/core/config.py is up-to-date."
+  echo >&2 "*** Latest defaults can be found at:"
+  echo >&2 "***   $SOURCE/core/config.default.py"
+  echo >&2 "*** "
+fi
--- script/setup-www.sh
+++ script/setup-www.sh
@@ -5,6 +5,7 @@ set -e
 WGET='wget -c -nv'
 SOURCE=$HOME/trait-o-matic
 TARGET=$HOME/www
+CONFIG=$HOME/config
 cd
 $WGET http://codeigniter.com/download.php
 rm -Rf CodeIgniter_1.7.1
@@ -21,5 +22,21 @@ cp -R $SOURCE/web/statistics $TARGET
 rm -Rf $TARGET/system/application
 cp -R $SOURCE/web/system/application $TARGET/system/application
 cp $SOURCE/web/htaccess $TARGET/.htaccess
+for conf in config database trait-o-matic
+do
+  ln -s $CONFIG/$conf.php $TARGET/system/application/config/$conf.php
+  if cp -i $TARGET/system/application/config/$conf.default.php $CONFIG/$conf.php
+  then
+    echo >&2 "*** "
+    echo >&2 "*** Please edit $CONFIG/$conf.php to suit your installation."
+    echo >&2 "*** "
+  else
+    echo >&2 "*** "
+    echo >&2 "*** Please ensure $CONFIG/$conf.php is up-to-date."
+    echo >&2 "*** Latest defaults can be found at:"
+    echo >&2 "***   $TARGET/system/application/config/$conf.default.php"
+    echo >&2 "*** "
+  fi
+done
 perl -p -e "s/%USER%/$USER/" < $SOURCE/web/system/application/config/upload.php.in > $TARGET/system/application/config/upload.php
 

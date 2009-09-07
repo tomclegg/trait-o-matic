35cdc44596d093285bd376a526d317775680adaf
Tom Clegg <tom@tomclegg.net>
2009-09-07 19:34:39 -0400

    add pharmgkb_stage1.pl from Mike, add perldoc sections

--- script/pharmgkb_stage2.pl
+++ script/pharmgkb_stage2.pl
@@ -1,5 +1,48 @@
 #!/usr/bin/perl
 
+=head1 USAGE
+
+ echo '
+  CREATE DATABASE `pharmgkb` DEFAULT CHARACTER SET ASCII COLLATE ascii_general_ci;
+  ' | mysql -uroot -p
+ (
+ set -e
+ set -o pipefail
+ echo "
+  DROP TABLE IF EXISTS pharmgkb.pharmgkb;
+  CREATE TABLE pharmgkb.pharmgkb (
+  chrom char(5),
+  pos int(10),
+  rsid char(16),
+  genotype char(3),
+  gene varchar(32),
+  amino_acid_change varchar(12),
+  pubmed_id varchar(64),
+  webresource varchar(255),
+  name char(48),
+  evidence text,
+  annotation text,
+  genes text,
+  drugs text,
+  drugclasses text,
+  diseases text,
+  unique(rsid,genotype,gene,amino_acid_change,annotation(177)),
+  index(rsid),
+  index(gene,amino_acid_change)
+  );
+  " | mysql -uroot -p
+ cat ~/Variant_annotation_filtered_allele_flipped{,_errs}.txt | ./pharmgkb_stage2.pl > ~/pharmgkb_import.tmp
+ echo "
+  DELETE FROM pharmgkb;
+  LOAD DATA LOCAL INFILE '~/pharmgkb_import.tmp'
+  INTO TABLE pharmgkb
+  FIELDS TERMINATED BY '\t'
+  LINES TERMINATED BY '\n';
+  " | mysql -uupdater -p pharmgkb
+ )
+
+=cut
+
 while (<>)
 {
     chomp;
@@ -91,46 +134,3 @@ while (<>)
 	print STDERR "skip: $_\n" if $ENV{DEBUG};
     }
 }
-
-__END__
-
-suggested usage:
-
-echo '
- CREATE DATABASE `pharmgkb` DEFAULT CHARACTER SET ASCII COLLATE ascii_general_ci;
- ' | mysql -uroot -p
-(
-set -e
-set -o pipefail
-echo "
- DROP TABLE IF EXISTS pharmgkb.pharmgkb;
- CREATE TABLE pharmgkb.pharmgkb (
- chrom char(5),
- pos int(10),
- rsid char(16),
- genotype char(3),
- gene varchar(32),
- amino_acid_change varchar(12),
- pubmed_id varchar(64),
- webresource varchar(255),
- name char(48),
- evidence text,
- annotation text,
- genes text,
- drugs text,
- drugclasses text,
- diseases text,
- unique(rsid,genotype,gene,amino_acid_change,annotation(177)),
- index(rsid),
- index(gene,amino_acid_change)
- );
- " | mysql -uroot -p
-cat ~/Variant_annotation_filtered_allele_flipped{,_errs}.txt | ./pharmgkb_stage2.pl > ~/pharmgkb_import.tmp
-echo "
- DELETE FROM pharmgkb;
- LOAD DATA LOCAL INFILE '~/pharmgkb_import.tmp'
- INTO TABLE pharmgkb
- FIELDS TERMINATED BY '\t'
- LINES TERMINATED BY '\n';
- " | mysql -uupdater -p pharmgkb
-)

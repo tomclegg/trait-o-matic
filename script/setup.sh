ac9b13d217faa3f72aae660ac9d0ef01bd3ec149
Tom Clegg <tom@tomclegg.net>
2009-10-11 19:59:28 -0400

    More installation tweaks

--- script/setup.sh
+++ script/setup.sh
@@ -24,7 +24,7 @@ sudo a2dissite default
 sudo /etc/init.d/apache2 restart
 
 # Init script
-perl -p -e 's/%([A-Z]+)%/$ENV{$1}/g' < $SOURCE_DIR/script/trait-o-matic.in | sudo tee /etc/init.d/trait-o-matic.tmp >/dev/null
+perl -p -e 's/%([A-Z]+)%/$ENV{$1}/g' < $SOURCE/script/trait-o-matic.in | sudo tee /etc/init.d/trait-o-matic.tmp >/dev/null
 sudo chmod 755 /etc/init.d/trait-o-matic.tmp
 sudo chown 0:0 /etc/init.d/trait-o-matic.tmp
 sudo mv /etc/init.d/trait-o-matic.tmp /etc/init.d/trait-o-matic

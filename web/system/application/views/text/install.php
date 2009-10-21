49f3e51d4a63bfd35c96df5ba84d955eb75f71e2
Tom Clegg <tom@tomclegg.net>
2009-10-21 14:48:50 -0400

    tweak docs

--- web/system/application/views/text/install.php
+++ web/system/application/views/text/install.php
@@ -4,7 +4,8 @@ h2. Installing from source
 
 To install the latest version of Trait-o-matic, issue the following commands.  _(This procedure was tested on Debian/GNU Linux version "lenny" and should work equally well on Ubuntu version "hardy" or later versions.)_
 
-bc. cd
+bc. (set -e
+cd
 chmod a+x ~
 sudo apt-get update
 sudo apt-get -qq install git git-core
@@ -17,22 +18,17 @@ USER=www-data HOME=/home/trait PORT=80 ./install-root.sh
 # Note prompt to set up a mysql root password during mysql-server install
 #
 sudo -u www-data USER=www-data HOME=/home/trait ./install-user.sh
+)
 
-Optionally, set up email so users can receive notifications when their
-jobs finish:
+Check the configuration (this should not output any warnings):
 
-bc. sudo apt-get install exim4
-
-Check the configuration:
-
-bc. cd ~/trait-o-matic/script
-./check.sh
+bc. ~/trait-o-matic/script/check.sh
 
 If your installation has access to a Free Factories storage system, set up the Free Factories client library now.  If the reference data is available on your cluster, this step will make the following step proceed _much_ faster.
 
 bc. echo "deb http://dev.freelogy.org/apt hardy main contrib non-free" \
  | sudo tee -a /etc/apt/sources.list
-wget -q http://dev.freelogy.org/53212765.key -O- |sudo apt-key add -
+wget -q http://dev.freelogy.org/53212765.key -O- | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install libwarehouse-perl
 
@@ -40,17 +36,13 @@ Configure the Free Factories client library:
 
 bc. [ -e /etc/warehouse/warehouse-client.conf ] || (
 sudo mkdir -p /etc/warehouse
-echo '
-   $Warehouse::warehouses=[{name=>"templeton",configurl=>
-   "http://templeton-controller.oxf.freelogy.org:44848/warehouse-client.conf"
-   }];
-   1;
-' | sudo tee /etc/warehouse/warehouse-client.conf >/dev/null
-)
+echo '$Warehouse::warehouses=[{name=>"templeton",configurl=>
+      "http://templeton-controller.oxf.freelogy.org:44848/warehouse-client.conf"}];1;
+     ' | sudo tee /etc/warehouse/warehouse-client.conf >/dev/null )
 
-Populate the databases with reference data.  _(Note: This takes about 30 minutes if you're downloading the data from the local cluster, or 2-3 if you're downloading everything on a fast internet link.)_
+Populate the databases with reference data.  _(Note: This takes about 30 minutes if you're downloading the data from the local cluster, or 2-3 hours if you're downloading everything on a fast internet link.)_
 
-bc. sudo -u www-data USER=www-data HOME=/home/trait IMPORT_BINARY=1 ./setup-external-data.sh
+bc. sudo -u www-data USER=www-data HOME=/home/trait IMPORT_BINARY=1 ~/trait-o-matic/setup-external-data.sh
 
 Finally, start the Trait-o-matic core XMLRPC server.
 

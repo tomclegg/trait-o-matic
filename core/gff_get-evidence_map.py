585b1acfa5d8dc973ed5f743cf967133362c00ac
Tom Clegg <tom@tomclegg.net>
2010-02-17 01:42:44 -0500

    putative>likely

--- core/gff_get-evidence_map.py
+++ core/gff_get-evidence_map.py
@@ -23,7 +23,7 @@ query = '''
 SELECT inheritance, impact, summary_short
 FROM latest
 WHERE gene=%s AND aa_change=%s
- AND impact NOT IN ('benign', 'putative benign', 'none')
+ AND impact NOT IN ('benign', 'likely benign', 'none')
  AND LENGTH(summary_short) > 0
 '''
 
